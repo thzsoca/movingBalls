8c623aadedee123b0cb398d9ffb06443b43a8d35
Thummerer Zsolt <thummerer.zsolt@gmail.com>
2021-10-09 17:20:45 +0200

    balls class created main Program created an error needs to be fixed

--- movingBalls/Program.cs
+++ movingBalls/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace movingBalls
 {
@@ -6,7 +7,27 @@ namespace movingBalls
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            balls a = new balls(10, 5, +1, +1); 
+            Thread t1 = new Thread(a.moves);
+            t1.Start();
+            balls b = new balls(30, 2, -1, +1);
+            Thread t2 = new Thread(b.moves);
+            t2.Start();
+            balls c = new balls(70, 10, -1, -1);
+            Thread t3 = new Thread(c.moves);
+            t3.Start();
+
+            bool stop = false;
+            while (!stop)
+            {
+                if (a.curposX == b.curposX && a.curposY == b.curposY || a.curposX == c.curposX && a.curposY == c.curposY || c.curposX == b.curposX && c.curposY == b.curposY)
+                {
+                    t1.Abort();
+                    t2.Abort();
+                    t3.Abort();
+                    stop = true;
+                }
+            }
         }
     }
 }
---
+++ movingBalls/balls.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace movingBalls
+{
+    class balls
+    {
+        public int curposX, curposY, intx, inty;
+        public balls(int curposX, int curposY, int intx, int inty)
+        {
+            this.curposX = curposX; this.curposY = curposY; this.intx = intx; this.inty = inty;
+        }  
+
+        public void moves()
+        {
+            while (true)
+            {
+                lock (typeof(Program))
+                {
+                    Console.SetCursorPosition(curposX, curposY);
+                    Console.Write(' ');
+                }
+                if (curposX < 80 && curposX > 0 && curposY < 25 && curposY > 0)
+                {
+                    curposX += intx;
+                    curposY += inty;
+                }
+                if (curposX == 0 || curposX == 80)
+                {
+                    intx *= -1;
+                    curposX += intx;
+                }
+                if (curposY == 0 || curposY == 25)
+                {
+                    intx *= -1;
+                    curposY += inty;
+                }
+
+                lock (typeof(Program))
+                {
+                    Console.SetCursorPosition(curposX, curposY);
+                    Console.Write('o');
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
