d75920f8f44b6eb11e8a81f165e7a47d00f8f090
Thummerer Zsolt <thummerer.zsolt@gmail.com>
2021-10-09 16:39:19 +0200

    setup

---
+++ movingBalls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace movingBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
